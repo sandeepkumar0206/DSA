f2d9572f51dcc99b375ca8675da7e858975d094b
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-01-05 21:05:03 +0530

    Day 1

---
+++ issorted.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int cnt=0;
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                cnt++;
+            }
+        }
+        if( nums[n-1]>nums[0]){
+            cnt++;
+        }
+        if(cnt>1){
+            return false;
+        }
+        return true;
+    }
+}
