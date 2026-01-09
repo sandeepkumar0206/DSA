77739c84fb6f796cff5bebdeacdefaddb737972f
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-01-09 21:02:46 +0530

    Day 5

---
+++ MaximumConsecutiveOnes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans=0;
+        int currMax=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                currMax++;
+            }
+            else{
+                ans=Math.max(ans,currMax);
+                currMax=0;
+            }
+        }
+        ans=Math.max(ans,currMax);
+        return ans;
+    }
+}
