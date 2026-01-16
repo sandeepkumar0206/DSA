94d1c88062b89284da40b67c068356fec0883194
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-01-16 16:59:49 +0530

    Day 11

---
+++ Power.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        double ans=1;
+        while(n!=0){
+            if((n&1)!=0){
+                ans=ans*x;
+            }
+            x*=x;
+            n=n>>>1;
+        }
+        return ans;
+    }
+}
