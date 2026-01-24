6759d04b0bf145ba0d2d2b2e809fdd51238ea122
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-01-24 22:38:22 +0530

    Day 19

---
+++ NumberOfSetBit.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+class Solution {
+    static int setBits(int n) {
+        // code here
+        int cnt=0;
+        while(n!=0){
+            if((n&1)!=0){
+                cnt++;
+            }
+            n>>=1;
+        }
+        return cnt;
+        
+    }
+}
