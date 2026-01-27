ac73d5dded689ac04891473275080c1fe5fc4ab8
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-01-27 19:24:52 +0530

    Day 22

---
+++ lowerBound.java
@@ -0,0 +1,20 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        int n=arr.length;
+        int s=0,e=n-1;
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(arr[m]<target){
+                s=m+1;
+            }
+            else{
+                e=m;
+            }
+        }
+        if(s==n-1 && arr[s]<target){
+            return n;
+        }
+        return s;
+    }
+}
