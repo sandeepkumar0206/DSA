9bfd8cb6679316665f7234e2c368218968ae95f0
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-01-30 20:52:40 +0530

    Day 25

---
+++ InsertionSort.java
@@ -0,0 +1,17 @@
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        // code here
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            int j=i-1;
+            int k=arr[i];
+            while(j>=0 && arr[j]>k){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=k;
+        }
+        
+    }
+}
