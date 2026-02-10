74ca9dcb04a99bb4b4b88b274ba343027280882a
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-02-10 22:57:40 +0530

    Day 36

---
+++ InsertAtHead.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getCount(Node head) {
+        int cnt=0;
+        while(head!=null){
+            head=head.next;
+            cnt++;
+        }
+        return cnt;
+    }
+}
