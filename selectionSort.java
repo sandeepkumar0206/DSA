db46223463753b70529f39dbc6dba8278d6db98d
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-02-14 13:05:42 +0530

    Day 40

---
+++ selectionSort.java
@@ -0,0 +1,20 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min]>=arr[j]){
+                    min=j;
+                }
+            }
+            if(min!=i){
+                int t=arr[min];
+                arr[min]=arr[i];
+                arr[i]=t;
+            }
+        }
+        
+    }
+}
