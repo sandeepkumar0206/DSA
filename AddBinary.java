034b9c252ca56064b258b4dc52da67fe6b0629b2
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-02-15 20:36:39 +0530

    ay 41

---
+++ AddBinary.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder ans=new StringBuilder("");
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        while(i>=0||j>=0||carry==1){
+            if(i>=0){
+                carry+=a.charAt(i--)-'0';
+            }
+            if(j>=0){
+                carry+=b.charAt(j--)-'0';
+            }
+            ans.append(carry%2);
+            carry/=2;
+        }
+        // int sum=0;
+        // int carry=0;
+        // while(i>=0||j>=0){
+        //     sum=carry;
+        //     if(i>=0){
+        //         sum+=a.charAt(i--)-'0';
+        //     }
+        //     if(j>=0){
+        //         sum+=b.charAt(j--)-'0';
+        //     }
+        //     if(sum>1){
+        //         carry=1;
+        //     }
+        //     else{
+        //         carry=0;
+        //     }
+        //     ans.append(sum%2);
+        // }
+        // if(carry==1){
+        //     ans.append("1");
+        // }
+        return ans.reverse().toString();
+    }
+}
