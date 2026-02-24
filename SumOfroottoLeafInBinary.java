0f8d13d93e091cd5aa501a682bab0c2c4a5aa701
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-02-24 19:08:54 +0530

    Day 50

---
+++ SumOfroottoLeafInBinary.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int ans=0;
+    public static void find(TreeNode root,String s){
+        if(root==null){
+            return ;
+        }
+        if(root.left==null && root.right==null){
+            int curr=get(s+root.val);
+            ans+=curr;
+        }
+        find(root.left,s+root.val);
+        find(root.right,s+root.val);
+    }
+    public static int get(String s){
+        int a=0,p=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)=='1'){
+                a+=Math.pow(2,p);
+            }
+            p++;
+        }
+        return a;
+    }
+    public int sumRootToLeaf(TreeNode root) {
+        ans=0;
+        find(root,"");
+        return ans;
+    }
+}
