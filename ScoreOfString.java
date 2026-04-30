e56e08e6a3c4ac857837aef717ce42a2867eb38e
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-04-30 23:10:17 +0530

    Day 99

---
+++ ScoreOfString.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int ans=0;
+        int n=s.length();
+        for(int i=1;i<n;i++){
+            ans+=Math.abs(s.charAt(i)-s.charAt(i-1));
+        }
+        return ans;
+    }
+}
