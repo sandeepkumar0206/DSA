e96e8eb186f208dd74abc47a12a734b8ec15a6db
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-08-06 17:28:26 +0000

    Day 141

--- Compute Decimal Representation.java
+++ Compute Decimal Representation.java
@@ -17,3 +17,24 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int[] decimalRepresentation(int n) {
+        int t=1;
+        List<Integer>li=new ArrayList<>();
+        while(n!=0){
+            int d=n%10;
+            if(d!=0)li.add(0,d*t);
+            t*=10;
+            n/=10;
+        }
+        // Collections.sort(li,Collections.reverseOrder());
+        int k=li.size();
+        int ans[]=new int[k];
+        for(int i=0;i<k;i++){
+            ans[i]=li.get(i);
+        }
+        return ans;
+    }
+}
