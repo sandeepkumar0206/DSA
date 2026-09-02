6aa1cc081e128407f75bfc56c1566cfc8a187d9d
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-09-02 18:23:06 +0000

    Day 146

---
+++ Construct Uniform Parity Array I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int cnt=0,n=nums1.length;int min=1;
+        // for(int i:nums1){
+        //     if(i%2==0){
+        //         cnt++;
+        //     }
+        //     min=Math.min(i,min);
+        // }g
+        // if(cnt==0){
+        //     return nums1;
+        // }
+        return true;
+    }
+}
